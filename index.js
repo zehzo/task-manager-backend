be5608d8c30e6f96ee9c0f7dafd3186ffa99801f
zehzo <edson.ferreiraj@hotmail.com>
2024-04-23 02:13:10 -0300

    feat: add route to get a task by id

--- index.js
+++ index.js
@@ -17,6 +17,21 @@ app.get('/tasks', async (req, res) => {
   }
 })
 
+app.get('/tasks/:id', async (req, res) => {
+  try {
+    const taskId = req.params.id
+    const task = await TaskModel.findById(taskId)
+    
+    if (!task){
+      return res.status(404).send('Esta tarefa não foi encontrada!')
+    }
+    res.status(200).send(task)
+
+  } catch (error) {
+    res.status(500).send(error.message)  
+  }
+})
+
 app.post('/tasks', async (req, res) => {
   try {
     const newTask = new TaskModel(req.body)
@@ -32,7 +47,7 @@ app.delete('/tasks/:id', async (req, res) => {
     const taskId = req.params.id
     const taskToDelete = await TaskModel.findById(taskId)
     if (!taskToDelete){
-      return res.status(500).send('Tarefa não encontrada')
+      return res.status(404).send('Tarefa não encontrada')
     }
     const deletedTask = await TaskModel.findByIdAndDelete(taskId)
     res.status(200).send(deletedTask)
