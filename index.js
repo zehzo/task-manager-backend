5ed4ac9b5684e722429d92110ecbad4d78b70baa
zehzo <edson.ferreiraj@hotmail.com>
2024-05-10 00:41:18 -0300

    feat: add cors

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors");
 const connectToDatabase = require("./src/database/mongoose.database");
 const TaskRouter = require("./src/routes/task.routes");
 
 dotenv.config();
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
 connectToDatabase();
